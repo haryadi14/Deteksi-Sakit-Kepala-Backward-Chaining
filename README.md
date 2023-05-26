ca4c8347d51773aad411e0470c5ce50c92ee57ea
haryadi <85815309+haryadi14@users.noreply.github.com>
2023-05-26 09:42:08 +0700

    UPDATE MAIN JS DAN README

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Deteksi-Sakit-Kepala-Backward-Chaining
+Sistem Pakar untuk mendiagnosis penyakit sakit kepala
+sistem pakar yang menggunakan metode inferensi backward chaining untuk mendiagnosis penyakit sakit kepala berdasarkan gejala yang dialami pasien. Berikut adalah penjelasan konsep-konsep yang digunakan dalam program tersebut:
+1.	Aturan sistem pakar
+Sistem pakar memanfaatkan aturan-aturan yang sudah didefinisikan sebelumnya untuk mendiagnosis penyakit. Dalam program ini, aturan-aturan tersebut didefinisikan dalam bentuk kamus Python, dengan setiap penyakit memiliki satu atau lebih aturan yang mengaitkan gejala-gejala dengan penyakit tersebut.
+
+2.	Gejala-gejala yang dialami pasien
+Gejala-gejala yang dialami pasien didefinisikan dalam bentuk daftar Python. Daftar ini kemudian digunakan oleh sistem pakar untuk mencocokkan gejala-gejala yang dialami pasien dengan aturan-aturan yang sudah didefinisikan sebelumnya. Contoh :
+• 1, Rasa nyeri terus menerus pada kedua sisi kepala
+  2, Terasa tekanan di belakang mata
+  3, Otot leher yang tegang
+  4, Adanya rasa mual
+  5, Rasa demam dan linglung
+  6, Pandangan kabur
+  7, Sakit kepala pada satu sisi
+  8, Kepala terasa sering berdenyut
+  9, Pening
+  10, Menjadi sensitif pada suara, bau, cahaya, atau sentuhan
+  11, Nyeri disertai sensasi terbakar atau tertusuk
+  12, Terasa sakit di sisi kepala yang sama
+  13, Sakit terpusat di belakang satu mata
+  14, Hidung tersumbat
+  15, Mata berair
+
+3.	Metode inferensi backward chaining
+Sistem pakar menggunakan metode inferensi backward chaining untuk mencari penyakit yang mungkin dialami pasien berdasarkan gejala-gejala yang dialami. Metode inferensi ini bekerja dengan cara memulai dari hipotesis akhir (yaitu penyakit yang ingin didiagnosis) dan mencari fakta-fakta yang dapat mendukung hipotesis tersebut, hingga didapatkan gejala-gejala yang dialami pasien. Dalam program ini, metode inferensi backward chaining dilakukan dengan fungsi backward_chaining.
+
+4.	Hasil diagnosis
+Setelah mencocokkan gejala-gejala yang dialami pasien dengan aturan-aturan yang sudah didefinisikan, sistem pakar akan memberikan diagnosis yang paling sesuai dengan gejala-gejala yang dialami pasien. Dalam program ini, diagnosis diberikan dalam bentuk teks yang ditampilkan di layar.
+
+Secara keseluruhan di atas menunjukkan cara sistem pakar dapat digunakan untuk mendiagnosis penyakit berdasarkan gejala yang dialami pasien. Program ini hanya merupakan contoh sederhana, dan dalam implementasi yang lebih kompleks, sistem pakar dapat mengandung lebih banyak aturan dan mempertimbangkan lebih banyak faktor dalam proses diagnosis.
+
+Nama Kelompok :
+1204004     -   JOSE CHASEY PRATAMA
+1204022     -   HANAN DESTRIARIN KISHENDRIAN
+1204025     -   CHRISTIAN YUDA PRATAMA
+1204026     -   HARYADI YUSUF
+
--- main.js
+++ main.js
@@ -1,52 +1,80 @@
 const HeadacheSymptoms = new Map([
-    [1, "Rasa nyeri terus menerus pada kedua sisi kepala"],
-    [2, "Terasa tekanan di belakang mata"],
-    [3, "Otot leher yang tegang"],
-    [4, "Adanya rasa mual"],
-    [5, "Rasa demam dan linglung"],
-    [6, "Pandangan kabur"],
-    [7, "Sakit kepala pada satu sisi"],
-    [8, "Kepala terasa sering berdenyut"],
-    [9, "Pening"],
-    [10, "Menjadi sensitif pada suara, bau, cahaya, atau sentuhan"],
-    [11, "Nyeri disertai sensasi terbakar atau tertusuk"],
-    [12, "Terasa sakit di sisi kepala yang sama"],
-    [13, "Sakit terpusat di belakang satu mata"],
-    [14, "Hidung tersumbat"],
-    [15, "Mata berair"]
-    ]) ;
-    
+  [1, "Rasa nyeri terus menerus pada kedua sisi kepala"],
+  [2, "Terasa tekanan di belakang mata"],
+  [3, "Otot leher yang tegang"],
+  [4, "Adanya rasa mual"],
+  [5, "Rasa demam dan linglung"],
+  [6, "Pandangan kabur"],
+  [7, "Sakit kepala pada satu sisi"],
+  [8, "Kepala terasa sering berdenyut"],
+  [9, "Pening"],
+  [10, "Menjadi sensitif pada suara, bau, cahaya, atau sentuhan"],
+  [11, "Nyeri disertai sensasi terbakar atau tertusuk"],
+  [12, "Terasa sakit di sisi kepala yang sama"],
+  [13, "Sakit terpusat di belakang satu mata"],
+  [14, "Hidung tersumbat"],
+  [15, "Mata berair"]
+]);
 
-const listAllSymtom = [...HeadacheSymptoms.keys()];
-const MigrainSymntom = [6,7,8,9,10];
-const TensionHeadacheSymntom = [1,2,3,4,5];
-const ClusterHeadacheSymntom = [11,12,13,14,15];
-
+const listAllSymptom = [...HeadacheSymptoms.keys()];
+const MigrainSymptom = [6, 7, 8, 9, 10];
+const TensionHeadacheSymptom = [1, 2, 3, 4, 5];
+const ClusterHeadacheSymptom = [11, 12, 13, 14, 15];
 
 function arraysEqual(a, b) {
     if (a.length !== b.length) {
-      return false;
+        return false;
     }
     for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        return false;
-      }
+        if (a[i] !== b[i]) {
+            return false;
+        }
     }
     return true;
-  };
-  
+}
+function checkSymptom(listSymptom) {
+  if (listSymptom.length < 5) {
+    return "Kamu tidak mengalami gejala sakit kepala";
+  }
 
-function checkSymptom(listSymntom) {
-    listSymntom.sort((a, b) => a - b)
-    if (arraysEqual(listSymntom, MigrainSymntom)){
-        return "Kamu mengalami Sakit Kepala dengan Jenis Migrain"
-    } else if (arraysEqual(listSymntom, TensionHeadacheSymntom)) {
-        return "Kamu mengalami Sakit Kepala dengan jenis Sakit Kepala Tegang"
-    } else if (arraysEqual(listSymntom, ClusterHeadacheSymntom)) {
-        return "Kamu mengalami Sakit Kepala dengan jenis Sakit Kepala Cluster"
-    } else if ( listSymntom.every(element => listAllSymtom.includes(element))) {
-        return "Kamu mengalami Sakit Kepala tapi kami tidak memiliki data memiliki data mengenai jenis apa sakit kepala tersebut"
-    } else {
-        return "Kamu tidak mengalami gejala gejala sakit kepala"
+  const hasMigrain = listSymptom.filter(symptom => MigrainSymptom.includes(symptom)).length === 5;
+  const hasTensionHeadache = listSymptom.filter(symptom => TensionHeadacheSymptom.includes(symptom)).length === 5;
+  const hasClusterHeadache = listSymptom.filter(symptom => ClusterHeadacheSymptom.includes(symptom)).length === 5;
+
+  if (hasMigrain && !hasTensionHeadache && !hasClusterHeadache) {
+    return "Kamu mengalami Sakit Kepala dengan Jenis Migrain";
+  } else if (!hasMigrain && hasTensionHeadache && !hasClusterHeadache) {
+    return "Kamu mengalami Sakit Kepala dengan jenis Sakit Kepala Tegang";
+  } else if (!hasMigrain && !hasTensionHeadache && hasClusterHeadache) {
+    return "Kamu mengalami Sakit Kepala dengan jenis Sakit Kepala Cluster";
+  } else {
+    return "Kamu tidak mengalami gejala sakit kepala";
+  }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const submitBtn = document.getElementById("submitBtn");
+  const modalElement = document.getElementById("modal");
+
+  submitBtn.addEventListener("click", function () {
+    const selectedOptions = [];
+    for (let i = 1; i <= 15; i++) {
+      const selectElement = document.getElementById("question" + i);
+      const selectedValue = selectElement.value;
+      if (selectedValue === "iya") {
+        selectedOptions.push(i);
+      }
     }
-};
+    const diagnosis = checkSymptom(selectedOptions);
+
+    modalElement.classList.remove("hidden");
+
+    modalElement.querySelector("#result").textContent = diagnosis;
+  });
+
+  modalElement.querySelector(".modal-close").addEventListener("click", function () {
+    modalElement.classList.add("hidden");
+  });
+});
